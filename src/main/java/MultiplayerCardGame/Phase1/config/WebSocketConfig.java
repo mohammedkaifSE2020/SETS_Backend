16ab84fee6a292890404d446e41a2cb606bfe505
mohammed <kaifmohammed008@gmail.com>
2026-02-02 22:36:38 +0530

    Updated WebSocketConfig file with frontend url

--- src/main/java/MultiplayerCardGame/Phase1/config/WebSocketConfig.java
+++ src/main/java/MultiplayerCardGame/Phase1/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package MultiplayerCardGame.Phase1.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -23,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // The URL the frontend connects to
         registry.addEndpoint("/ws-game")
-                .setAllowedOrigins("http://localhost:5173") // Your React Port
+                .setAllowedOrigins(frontendUrl) // Your React Port
                 .withSockJS(); // Fallback for older browsers
     }
 }
